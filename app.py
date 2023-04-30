e7422178bd7d5dee8de85512100165e33b6d4ca3
Samarth Agali Prasad <samarthprasad440@gmail.com>
2023-04-30 13:35:04 +0530

    the app with try except

--- app.py
+++ app.py
@@ -27,108 +27,25 @@ def get_images(prompt,n):
     size="512x512"
     )
     i=0
-    for image in response['data']:
-        response = requests.get(image['url'])
-    # Convert the image data to a PIL image and save it to your computer
-        image_data = response.content
-        image = Image.open(BytesIO(image_data))
-        # image.show()
-        image.save("GEN_IMG"+str(i)+".png")
-    # files.download(prompt_text+str(i)+".png")
-        i+=1
-
+    try:
+        for image in response['data']:
+            response = requests.get(image['url'])
+        # Convert the image data to a PIL image and save it to your computer
+            image_data = response.content
+            image = Image.open(BytesIO(image_data))
+            # image.show()
+            image.save("GEN_IMG"+str(i)+".png")
+        # files.download(prompt_text+str(i)+".png")
+            i+=1
+    except:
+        print(" error in generating images.Please refresh the page ")
 def display(prompt,n):
     get_images(prompt,n)
-    for i in range(0,n):
-            st.image("GEN_IMG"+str(i)+".png")
-
-# def ppt_generation():
-
-#     # open presentation template
-#     # pptx = 'template.pptx'
-#     prs = Presentation()
-#     title_slide_layout = prs.slide_layouts[0]
-#     slide = prs.slides.add_slide(title_slide_layout)
-#     title = slide.shapes.title
-#     subtitle = slide.placeholders[1]
-
-#     title.text = prompt
-#     subtitle.text = "That's what its about!"
-#     genTitles,genText = read_text()
-
-    
-#     slideCount = len(genTitles)
-#     for i in range(slideCount):
-#         #Slide Images
-#         blank_slide_layout = prs.slide_layouts[1]
-#         slide = prs.slides.add_slide(blank_slide_layout)
-
-#         slideTitle = slide.placeholders[0]
-#         slideContent = slide.placeholders[1]
-
-#         slideTitle.text = genTitles[i]
-#         slideContent.size = Pt(20)
-#         slideContent.text = genText[i]
-
-
-        
-
-        # left = top = width = height = Inches(1)
-        # # txBox = slide.shapes.add_textbox(left, top, width, height)
-        # txBox = slide.shapes.add_textbox(left, top, prs.slide_width -Inches(2), height)
-        # tf = txBox.text_frame
-
-        # img_path = "./imgs/generated_image"+str((i)%7+1)+".jpg"
-        # pic = slide.shapes.add_picture(img_path, prs.slide_width - Inches(2.5), 0,Inches(2))
-        
-        
-
-        # p = tf.add_paragraph()
-        # p.text = genTitles[i]
-        # p.font.size = Pt(40)
-        # p.font.bold = True
-
-        # pic = slide.shapes.add_picture(img_path, prs.slide_width - Inches(5), Inches(1),)
-        # # tf.text = "This is text inside a textbox"
-        # txBox = slide.shapes.add_textbox(left, top+Inches(3), prs.slide_width -Inches(2), height)
-        # tf = txBox.text_frame
-        # p = tf.add_paragraph()  
-        # p.text = genText[i]
-
-
-
-    # # save presentation as binary output
-    # binary_output = BytesIO()
-    # prs.save(binary_output) 
-
-    # # display success message and download button
-    # st.success('The slides have been generated!')
-    
-    # return binary_output
-
-def read_text():
-    with open('text_gen.txt') as f:
-        titles = []
-        contents = []
-        lines = f.readlines()
-        for line in  lines:
-            if line == " " or line == "":
-                continue
-            # st.write(line)
-            l = line.split(" ", 1)
-            if len(l) < 2:
-                continue
-            # st.write(l)
-            titles.append(l[0])
-            contents.append(l[1])
-        return [titles,contents]
-    
-
-
-st.set_page_config(layout="wide")
-
-# st.title("Team ABS")
-# st.header("prompt")
+    try:
+        for i in range(0,n):
+                st.image("GEN_IMG"+str(i)+".png")
+    except:
+        print("error in loading image files.Please refresh the page")
 
 a = st.empty()
 
